355d519a2dc0ae09ccafa19d6d5257c286d47db4
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-12 14:53:28 -0700

    Create src.py

---
+++ src.py
@@ -0,0 +1,25 @@
+def isMagic(n): 
+    sum = 0; 
+    while (n > 0 or sum > 9): 
+        if (n == 0): 
+            n = sum; 
+            sum = 0; 
+        sum = sum + n % 10; 
+        n = int(n / 10); 
+    return True if (sum == 1) else False; 
+ 
+ 
+n = 1234; 
+if (isMagic(n)): 
+    print("Magic Number"); 
+else: 
+    print("Not a magic Number");
+    
+    
+    
+     # Note that the loop  
+    # continues if n is 0  
+    # and sum is non-zero. 
+    # It stops when n becomes  
+    # 0 and sum becomes single 
+    # digit. 
