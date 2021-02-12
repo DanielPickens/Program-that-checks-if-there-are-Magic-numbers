1646b2a2ccd99d49c9258c7eedbf4c38a4aa0f29
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-12 15:00:09 -0700

    Update src.py

--- src.py
+++ src.py
@@ -1,16 +1,16 @@
-def isMagic(n): 
+def isMagic(cool_variable): 
     sum = 0; 
-    while (n > 0 or sum > 9): 
-        if (n == 0): 
-            n = sum; 
+    while (cool_variable > 0 or sum > 9): 
+        if (cool_variable == 0): 
+            cool_variable = sum; 
             sum = 0; 
-        sum = sum + n % 10; 
-        n = int(n / 10); 
+        sum = sum + cool_variable % 10; 
+        cool_variable = int(cool_variable / 10); 
     return True if (sum == 1) else False; 
  
  
-n = 1234; 
-if (isMagic(n)): 
+cool_variable = 1234; 
+if (isMagic(cool_variable)): 
     print("Magic Number"); 
 else: 
     print("Not a magic Number");
@@ -18,8 +18,8 @@ else:
     
     
      # Note that the loop  
-    # continues if n is 0  
+    # continues if Cool_variable is 0  
     # and sum is non-zero. 
-    # It stops when n becomes  
+    # It stops when Cool_variable becomes  
     # 0 and sum becomes single 
     # digit. 
